7fe84aa84f88777503c893ec540b0fbb81567526
Nithya Prakash <nithyap322@gmail.com>
2021-09-02 12:26:12 +0530

    added my solution to the solution

--- server.js
+++ server.js
@@ -24,7 +24,24 @@ app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
 });
 
+app.get("/api", function (req, res) {
+  res.json({ unix: Date.now(), utc: Date() });
+});
 
+app.get("/api/:date", function (req, res) {
+  let dateString = req.params.date
+  if(/\d{5,}/.test(dateString)) {
+    let dateInteger = parseInt(dateString);
+    res.json({unix: dateString, utc: new Date(dateInteger).toUTCString()});
+  }
+  let dateObj = new Date(dateString);
+  if(dateObj.toString() === 'Invalid Date'){
+    res.json({error: 'Invalid Date'});
+  } else{
+    res.json({ 'unix': dateObj.valueOf(), 
+    'utc': dateObj.toUTCString() });
+  }
+});
 
 // listen for requests :)
 var listener = app.listen(process.env.PORT, function () {
